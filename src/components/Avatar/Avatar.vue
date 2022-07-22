92c8a4224ff8ce9c0030047fbdc1963ec95d1476
Alexandr <a.pashkevich22@gmail.com>
2022-07-22 12:07:21 +0300

    feat: some improvements for npm publish

--- src/components/Avatar/Avatar.vue
+++ src/components/Avatar/Avatar.vue
@@ -1,5 +1,7 @@
 <template>
-  <img class="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="Rounded avatar">
+  <div>
+<!--    <img class="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="Rounded avatar">-->
+  </div>
 </template>
 <script lang="ts" setup>
 import { computed, toRefs } from 'vue'
