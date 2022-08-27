0ebf7d9a0373dac9f4db2adf2437a1959e74750d
cadiyak <cadiyak@hotmail.fr>
2022-08-27 15:41:36 +0200

    feat: start to implement carousel.

---
+++ docs/components/carousel/examples/CarouselNoControlsExample.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="vp-raw flex flex-col">
+    <Carousel :controls="false"></Carousel>
+  </div>
+</template>
+<script setup>
+import { Carousel } from '../../../../src/index'
+</script>
--- src/components/Carousel/Carousel.vue
+++ src/components/Carousel/Carousel.vue
@@ -1,35 +1,26 @@
 <template>
-  <div id="default-carousel" class="relative" data-carousel="static">
+  <div id="default-carousel" class="relative">
     <!-- Carousel wrapper -->
     <div class="overflow-hidden relative h-56 rounded-lg sm:h-64 xl:h-80 2xl:h-96">
       <!-- Item 1 -->
-      <div class="hidden duration-700 ease-in-out" data-carousel-item>
-        <span class="absolute top-1/2 left-1/2 text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 sm:text-3xl dark:text-gray-800">First Slide</span>
-<!--        <img src="/docs/images/carousel/carousel-1.svg" class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="...">-->
-      </div>
-      <!-- Item 2 -->
-      <div class="hidden duration-700 ease-in-out" data-carousel-item>
-<!--        <img src="/docs/images/carousel/carousel-2.svg" class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="...">-->
-      </div>
-      <!-- Item 3 -->
-      <div class="hidden duration-700 ease-in-out" data-carousel-item>
-<!--        <img src="/docs/images/carousel/carousel-3.svg" class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="...">-->
+<!--      duration-700 ease-in-out-->
+      <div :class="index === currentPicture ? 'z-30' : 'z-0'"
+           v-for="(picture, index) in pictures" :key="picture" class="absolute inset-0 -translate-y-0">
+        <img :src="picture.src" class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" :alt="picture.alt">
       </div>
     </div>
     <!-- Slider indicators -->
-    <div class="flex absolute bottom-5 left-1/2 z-30 space-x-3 -translate-x-1/2">
-      <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-      <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-      <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+    <div v-if="indicators" class="flex absolute bottom-5 left-1/2 z-30 space-x-3 -translate-x-1/2">
+      <button v-for="(picture, index) in pictures" :key="picture" type="button" :class="index === currentPicture ? 'bg-white' : 'bg-white/50'" class="w-3 h-3 rounded-full bg-white" aria-current="false" :aria-label="'Slide ' + index" @click.prevent="slideTo(index)"></button>
     </div>
     <!-- Slider controls -->
-    <button type="button" class="flex absolute top-0 left-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-prev>
+    <button v-if="controls" @click.prevent="previousPicture" type="button" class="flex absolute top-0 left-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-prev>
       <span class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
         <svg class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
         <span class="hidden">Previous</span>
       </span>
     </button>
-    <button type="button" class="flex absolute top-0 right-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-next>
+    <button v-if="controls" @click.prevent="nextPicture" type="button" class="flex absolute top-0 right-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-next>
       <span class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
         <svg class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
         <span class="hidden">Next</span>
@@ -38,28 +29,85 @@
   </div>
 </template>
 <script lang="ts" setup>
-import { computed, toRefs } from 'vue'
-import type { PropType } from 'vue'
+import { onMounted, ref } from 'vue'
 
 const props = defineProps({
-  children: {
+  pictures: {
     type: Array,
     default() {
-      return []
+      return [
+        {
+          'src': 'https://flowbite.com/docs/images/carousel/carousel-1.svg',
+          'alt': 'Picture 1',
+        },
+        {
+          'src': 'https://flowbite.com/docs/images/carousel/carousel-2.svg',
+          'alt': 'Picture 2',
+        },
+        {
+          'src': 'https://flowbite.com/docs/images/carousel/carousel-3.svg',
+          'alt': 'Picture 3',
+        },
+      ]
     },
   },
   indicators: {
     type: Boolean,
     default: true,
   },
-  slide: {
+  controls: {
     type: Boolean,
     default: true,
+  },
+  slide: {
+    type: Boolean,
+    default: false,
   },
   slideInterval: {
     type: Number,
     default: 3000,
   },
+  animation: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const currentPicture = ref(0)
+const direction = ref('')
+
+const automaticSlide = () => {
+    setInterval(function() {
+      nextPicture()
+    }, props.slideInterval)
+}
+
+const slideTo = (index: number) => {
+  currentPicture.value = index
+}
+
+const nextPicture = () => {
+  if (currentPicture.value !== props.pictures.length - 1) {
+    currentPicture.value ++
+  } else {
+    currentPicture.value = 0
+  }
+  direction.value = 'right'
+}
+
+const previousPicture = () => {
+  if (currentPicture.value !== 0) {
+    currentPicture.value --
+  } else {
+    currentPicture.value = props.pictures.length -1
+  }
+  direction.value = 'left'
+}
+
+onMounted(() => {
+  if (props.slide) {
+    automaticSlide()
+  }
 })
 
 </script>
