8d3390c44099237df28b75bd5576832cb483e40f
Dmitri <19738862+Dmitri-2@users.noreply.github.com>
2022-09-01 02:54:29 -0700

    Make title and footer conditional

--- src/components/Modal/Modal.vue
+++ src/components/Modal/Modal.vue
@@ -2,12 +2,13 @@
   <div>
     <slot name="trigger" :show="showModal" :hide="hideModal" :toggle="toggleModal"></slot>
     <div v-if="!isHidden" class="bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40"></div>
-    <div v-if="!isHidden" id="defaultModal" tabindex="-1" :aria-hidden="isHidden ? 'true' : 'false'" class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center flex">
+    <div v-if="!isHidden" tabindex="-1" :aria-hidden="isHidden ? 'true' : 'false'" class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center flex">
       <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
           <!-- Modal header -->
-          <div class="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
+          <div class="flex justify-between items-start p-4 rounded-t dark:border-gray-600"
+               :class="$slots.title ? 'border-b' : ''">
             <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
               <slot name="title"></slot>
             </h3>
@@ -16,11 +17,11 @@
             </button>
           </div>
           <!-- Modal body -->
-          <div class="p-6 space-y-6">
+          <div class="p-6 space-y-6" :class="$slots.title ? '' : 'pt-0'">
             <slot name="body" :show="showModal" :hide="hideModal" :toggle="toggleModal"></slot>
           </div>
           <!-- Modal footer -->
-          <div class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
+          <div v-if="$slots.footer" class="flex items-center p-6 space-x-2 rounded-b border-gray-200 dark:border-gray-600">
             <slot name="footer"  :show="showModal" :hide="hideModal" :toggle="toggleModal"/>
           </div>
         </div>
