d7daf5b4d92936dde3b19ab024f786ca127a573b
Ilya Sosidka <i.sosidka@nlogic.ai>
2022-09-03 11:53:40 +0300

    chore: Added types for avatar component

---
+++ src/components/Avatar/types.ts
@@ -0,0 +1,3 @@
+export type AvatarSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+export type AvatarStatus = 'away' | 'busy' | 'offline' | 'online'
+export type AvatarStatusPosition = 'bottom-left' | 'bottom-right' | 'bottom-center' | 'top-left' | 'top-center' | 'top-right' | 'center-left' | 'center' | 'center-right'
