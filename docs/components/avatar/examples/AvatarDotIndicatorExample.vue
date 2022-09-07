4f274a907ee4b659dcb3a81ab00c8ae4c1eb5687
Ilya Artamonov <ilya.sosidka@gmail.com>
2022-09-07 12:54:04 +0300

    fix: Fixed offline status in an example component

--- docs/components/avatar/examples/AvatarDotIndicatorExample.vue
+++ docs/components/avatar/examples/AvatarDotIndicatorExample.vue
@@ -3,7 +3,7 @@
     <Avatar img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" class="mr-2.5" status="online" />
     <Avatar img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" class="mr-2.5" status="busy" />
     <Avatar img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" class="mr-2.5" status="away" />
-    <Avatar img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" class="mr-2.5" status="online" />
+    <Avatar img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" class="mr-2.5" status="offline" />
   </div>
 </template>
 <script setup>
