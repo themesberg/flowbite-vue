8f27fb03fd20941512473538429c2d8bd4541eac
Ilya Artamonov <hire@ilya-artamonov.ru>
2022-11-17 21:05:24 +0300

    feat: Added Navbar component

--- docs/components/navbar/examples/NavbarActionButtonExample.vue
+++ docs/components/navbar/examples/NavbarActionButtonExample.vue
@@ -8,11 +8,14 @@
       </template>
       <template #default="{isShowMenu}">
         <NavbarCollapse :isShowMenu="isShowMenu">
-          <NavbarLink isActive link="#">Home</NavbarLink>
-          <NavbarLink link="#">Services</NavbarLink>
-          <NavbarLink link="#">Pricing</NavbarLink>
-          <NavbarLink link="#">Contact</NavbarLink>
+          <NavbarLink is-active>Home</NavbarLink>
+          <NavbarLink>Services</NavbarLink>
+          <NavbarLink>Pricing</NavbarLink>
+          <NavbarLink>Contact</NavbarLink>
         </NavbarCollapse>
+      </template>
+      <template #right-side>
+        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
       </template>
     </Navbar>
   </div>
