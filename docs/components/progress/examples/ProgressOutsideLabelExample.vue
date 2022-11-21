aba9da556e9fc2f801625bfc0653e4ab4c9f8d6f
Ilya Artamonov <hire@ilya-artamonov.ru>
2022-11-21 21:08:52 +0300

    fix: Path fixes

--- docs/components/progress/examples/ProgressOutsideLabelExample.vue
+++ docs/components/progress/examples/ProgressOutsideLabelExample.vue
@@ -2,5 +2,5 @@
   <Progress labelProgress="true" labelPosition="outside" label="Flowbite Vue 3" progress="45"></Progress>
 </template>
 <script setup>
-import { Progress } from 'flowbite-vue'
+import { Progress } from '../../../../src/index'
 </script>
