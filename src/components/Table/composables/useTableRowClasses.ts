8b619ae08c74ce53305abc7677d1473b08f87d56
victor <viktor.generalov@nlogic.ai>
2022-12-13 20:39:40 +0400

    fix: fixed striped rows

--- src/components/Table/composables/useTableRowClasses.ts
+++ src/components/Table/composables/useTableRowClasses.ts
@@ -3,7 +3,7 @@ import type { Ref } from 'vue'
 import classNames from 'classnames'
 
 const baseClasses = 'bg-white dark:bg-gray-800 [&:not(:last-child)]:border-b [&:not(:last-child)]:dark:border-gray-700'
-const stripedClasses = 'odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-800'
+const stripedClasses = 'odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700'
 const hoverableClasses = 'hover:bg-gray-50 dark:hover:bg-gray-600'
 
 export function useTableRowClasses(): { tableRowClasses: Ref<string> } {
