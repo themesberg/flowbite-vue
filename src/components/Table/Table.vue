060fa3a6205f46af914c9b4c92c5bfc5e73b6bcb
Ilya Artamonov <ilya.sosidka@gmail.com>
2022-12-18 13:41:20 +0300

    Update src/components/Table/Table.vue

--- src/components/Table/Table.vue
+++ src/components/Table/Table.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="vp-raw relative overflow-x-auto shadow-md sm:rounded-lg">
+  <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
       <slot></slot>
     </table>
