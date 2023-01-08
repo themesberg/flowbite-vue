1180aedee0744b9b372efb534ce2a4f49a88c556
hirakei1203 <hirano.keitaro.64x@gmail.com>
2023-01-08 23:26:47 +0900

    make footer component basic

--- docs/components/footer/examples/FooterExample.vue
+++ docs/components/footer/examples/FooterExample.vue
@@ -1,8 +1,28 @@
 <template>
   <div class="vp-raw flex flex-col">
-    <Footer></Footer>
+    <Footer>
+      <FooterCopyright>
+        © 2022 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+      </FooterCopyright>
+      <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+          <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+        </li>
+        <li>
+          <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+        </li>
+        <li>
+          <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
+        </li>
+        <li>
+          <a href="#" class="hover:underline">Contact</a>
+        </li>
+      </ul>
+      </Footer>
   </div>
 </template>
+
 <script setup>
-import { Footer } from '../../../../src/index'
+// import { Footer  } from '../../../../src/index'
+import { Footer, FooterCopyright, FooterLinkGroup, FooterLink } from '../../../../src/index'
 </script>
---
+++ src/components/Footer/FooterCopyright.vue
@@ -0,0 +1,5 @@
+<template>
+  <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+    <slot></slot>
+  </span>
+</template>
