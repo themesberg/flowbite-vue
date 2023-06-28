8e00eba796c843f5fade2677996ce74f8bbeb975
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2023-06-28 18:42:02 +0300

    feature(analytics): add privacy friendly fathom analytics

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -87,6 +87,29 @@ export default defineConfig({
   cleanUrls: 'without-subfolders',
   head: [
     ['link', { rel: "icon", type: "image/svg", href: "/assets/logo.svg"}],
+    [
+      "script",
+      {},
+      `
+      // Fathom - beautiful, simple website analytics
+      (function(f, a, t, h, o, m) {
+        a[h] =
+          a[h] ||
+          function() {
+            (a[h].q = a[h].q || []).push(arguments);
+          };
+        (o = f.createElement("script")),
+          (m = f.getElementsByTagName("script")[0]);
+        o.async = 1;
+        o.src = t;
+        o.id = "fathom-script";
+        m.parentNode.insertBefore(o, m);
+      })(document, window, "https://cdn.usefathom.com/tracker.js", "fathom");
+      fathom("set", "siteId", "MPNTKCVJ");
+      fathom("trackPageview");
+      // / Fathom
+      `
+    ]
   ],
   themeConfig: {
     sidebar: buildSidebar(),
