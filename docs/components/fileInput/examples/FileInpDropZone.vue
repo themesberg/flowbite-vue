9ee674e732a36f69c529169c2505480499e4e796
vasu <vasucp1207@gmail.com>
2023-07-08 17:06:25 +0530

    dropzone value update

--- docs/components/fileInput/examples/FileInpDropZone.vue
+++ docs/components/fileInput/examples/FileInpDropZone.vue
@@ -1,11 +1,17 @@
 <template>
-  <FileInput dropzone="true">
+  <div>
+  <FileInput v-model="file" dropzone="true">
     <p class="!mt-1 text-xs text-gray-500 dark:text-gray-400">
       SVG, PNG, JPG or GIF (MAX. 800x400px)
     </p>
   </FileInput>
+  File: {{ file }}
+</div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import FileInput from '../../../../src/components/FileInput/FileInput.vue'
+
+const file = ref('')
 </script>
--- src/components/FileInput/FileInput.vue
+++ src/components/FileInput/FileInput.vue
@@ -15,10 +15,13 @@
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
               d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
           </svg>
-          <p :class="dropzoneTextClasses"><span class="font-semibold">Click to upload</span> or
-            drag and drop
-          </p>
-          <slot />
+          <div v-if="!model">
+            <p :class="dropzoneTextClasses"><span class="font-semibold">Click to upload</span> or
+              drag and drop
+            </p>
+            <slot />
+          </div>
+          <p v-else>value: {{ model }}</p>
         </div>
         <input id="dropzone-file" type="file" class="hidden" />
       </label>
