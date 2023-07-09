28d0ecff27ec1ab69a17f11b1c5767df879bff0d
vasu <vasucp1207@gmail.com>
2023-07-09 05:35:37 +0530

    textarea

---
+++ docs/components/textarea/examples/DefaultTextarea.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <Textarea rows="4" placeholder="Write you message..." v-model="message" label="Your message" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import Textarea from '../../../../src/components/Textarea/Textarea.vue'
+
+const message = ref('')
+</script>
