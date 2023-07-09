ff4564767bc553a9df940a41f5660fe0cea35f6c
Nick Adams <nickolas.j.adams@gmail.com>
2023-07-08 23:27:20 -0600

    feat(modal-property): additional exit functionality
    Adds a new property `escapable` to the modal component.
    This property is set to true by default to improve
    accesibility and user experience.
    
    New ways to close the modal
      - pressing the escape key
      - clicking outside of the modal
    
    It is sometimes important that a user interact with the content
    in a modal before continuing. Maybe a terms of service, or age
    verification. Setting the escapable property to false allows the
    developer to close the modal only after the user meets certain
    requirements. In this case, the escape key, outside exit clicks,
    and the close button in the modal header are removed.
    
    fix(accessibility): aria-label the close button for screenreaders
    
    docs: adds an example and escapable info section to documentation

--- docs/components/modal/examples/ModalExample.vue
+++ docs/components/modal/examples/ModalExample.vue
@@ -3,7 +3,7 @@
     <button @click="showModal" type="button" class="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
       {{ triggerText }}
     </button>
-    <Modal :size="size" v-if="isShowModal" @close="closeModal">
+    <Modal :escapable="escapable" :size="size" v-if="isShowModal" @close="closeModal">
       <template #header>
         <div class="flex items-center text-lg">
           Terms of Service
@@ -58,6 +58,10 @@ defineProps({
   size: {
     type: String as PropType<ModalSize>,
     default: '2xl',
+  },
+  escapable: {
+    type: Boolean,
+    default: true,
   },
   triggerText: {
     type: String,
