29eaa5e48edd615c2eb3fb3dceeac39f88975036
Ilya Artamonov <ilya.sosidka@gmail.com>
2023-09-08 20:05:51 +0300

    Update docs/components/input/examples/InputValidationExample.vue

--- docs/components/input/examples/InputValidationExample.vue
+++ docs/components/input/examples/InputValidationExample.vue
@@ -2,7 +2,7 @@
   <div class="vp-raw">
     <Input v-model='email' required placeholder="enter your email address" label="Email" validation-status='success' />
     <hr class='mt-4 border-0'>
-    <Input v-model='email' required placeholder="enter your email address" label="Email" validationStatus='error'>
+    <Input v-model='email' required placeholder="enter your email address" label="Email" validation-status='error'>
       <template #validationMessage>
         Please enter a valid email address
       </template>
