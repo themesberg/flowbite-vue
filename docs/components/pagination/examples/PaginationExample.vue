96b735b0a921d85169e6559dceaac20eb97b5217
gassio <gassioglou@gmail.com>
2023-09-15 16:31:54 +0300

    Adds page change event (#189)
    Adds first and last pagination items
    
    Co-authored-by: gassio <gassiogi@gmail.com>

--- docs/components/pagination/examples/PaginationExample.vue
+++ docs/components/pagination/examples/PaginationExample.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="vp-raw">
-    <Pagination v-model="currentPage" :total-pages="100"></Pagination>
+    <Pagination v-model="currentPage" :total-pages="100" @page-changed="handlePageChange"></Pagination>
   </div>
 </template>
 <script lang="ts" setup>
@@ -8,4 +8,8 @@ import { Pagination } from '../../../../src/index'
 import { ref } from 'vue'
 
 const currentPage = ref<number>(1)
+
+const handlePageChange = (page: number) => {
+  console.log('page changed', page)
+}
 </script>
