674a3f9486379dcef43b1ba9dd1eeb4949283069
Sqrcz <naorniakowski@slashlab.pl>
2023-09-20 13:33:36 +0200

    refactor: stricter linting  + dependencies updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,29 @@
+require('@rushstack/eslint-patch/modern-module-resolution')
+
 module.exports = {
   env: {
     browser: true,
     es2021: true,
     node: true,
   },
-  parser: 'vue-eslint-parser',
   extends: [
     'eslint:recommended',
-    'plugin:vue/essential',
-    'plugin:@typescript-eslint/recommended',
+    'standard',
+    'plugin:vue/vue3-recommended',
+    'plugin:vue-scoped-css/vue3-recommended',
+    '@vue/eslint-config-typescript/recommended'
   ],
   parserOptions: {
     ecmaVersion: 12,
-    parser: '@typescript-eslint/parser',
     sourceType: 'module',
   },
-  plugins: [
-    'vue',
-    '@typescript-eslint',
-  ],
   rules: {
     'comma-dangle': ['error', 'always-multiline'],
     'linebreak-style': ['error', 'unix'],
     'no-multiple-empty-lines': 'error',
     'object-curly-spacing': ['error', 'always'],
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'never'],
-    'vue/multi-word-component-names': 'off',
-    'vue/no-v-model-argument': 'off',
+    'sort-imports': ['error', { ignoreDeclarationSort: true }],
+    'vue/block-order': ['error', { order: ['template', 'script', 'style'] }],
+    'vue/component-name-in-template-casing': ['error', 'kebab-case'],
   },
 }
