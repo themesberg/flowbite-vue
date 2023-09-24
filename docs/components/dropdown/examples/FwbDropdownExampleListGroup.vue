e0d0e734c15e0b3046fb9497d89aab6f6173dc5c
Sqrcz <naorniakowski@slashlab.pl>
2023-09-24 19:07:08 +0200

    refactoring: dropdown component  - component  - docs

---
+++ docs/components/dropdown/examples/FwbDropdownExampleListGroup.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="vp-raw">
+    <fwb-dropdown text="Menu">
+      <list-group>
+        <list-group-item>
+          Profile
+        </list-group-item>
+        <list-group-item>
+          Settings
+        </list-group-item>
+        <list-group-item>
+          Messages
+        </list-group-item>
+        <list-group-item>
+          Download
+        </list-group-item>
+      </list-group>
+    </fwb-dropdown>
+  </div>
+</template>
+
+<script setup>
+import { FwbDropdown, ListGroup, ListGroupItem } from '../../../../src/index'
+</script>
--- src/components/Dropdown/types.ts
+++
@@ -1,1 +0,0 @@
-export type DropdownPlacement = 'top' | 'bottom' | 'left' | 'right'
