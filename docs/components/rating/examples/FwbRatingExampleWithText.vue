6341aae5816376ab9d233764897e0c9f8b9c1d3b
Sqrcz <naorniakowski@slashlab.pl>
2023-10-01 19:24:21 +0200

    refactoring: rating component  - component  - docs

---
+++ docs/components/rating/examples/FwbRatingExampleWithText.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="vp-raw">
+    <fwb-rating :rating="4.75">
+      <template #besideText>
+        <p class="ml-2 text-sm font-medium text-gray-500 dark:text-gray-400">
+          4.75 out of 5
+        </p>
+      </template>
+    </fwb-rating>
+  </div>
+</template>
+
+<script setup>
+import { FwbRating } from '../../../../src/index'
+</script>
--- docs/components/rating/examples/RatingExample.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="vp-raw flex flex-col">
-    <Rating :rating="4" />
-  </div>
-</template>
-<script setup>
-import { Rating } from '../../../../src/index'
-</script>
