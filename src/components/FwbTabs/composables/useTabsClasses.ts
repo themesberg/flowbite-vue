b2176885585cb9822fc22f83a54c859f7158c10f
Ilya Artamonov <hire@ilya-artamonov.ru>
2023-10-19 19:14:27 +0300

    fix: Fixed tabs

--- src/components/FwbTabs/composables/useTabsClasses.ts
+++ src/components/FwbTabs/composables/useTabsClasses.ts
@@ -1,5 +1,6 @@
 import { computed, type Ref } from 'vue'
 import type { TabsVariant } from '../types'
+import { twMerge } from 'tailwind-merge'
 
 export type UseTabsClassesProps = {
   variant: TabsVariant
@@ -12,15 +13,11 @@ export function useTabsClasses (props: UseTabsClassesProps): {
   const ulClasses = computed(() => {
     const baseClasses = 'flex flex-wrap font-medium text-center text-gray-500 dark:text-gray-400 text-sm'
 
-    if (props.variant === 'default') {
-      return `${baseClasses} border-b border-gray-200 dark:border-gray-700`
-    } else if (props.variant === 'pills') {
-      return ''
-    } else if (props.variant === 'underline') {
-      return '-mb-px'
-    }
-
-    return ''
+    return twMerge(
+      baseClasses,
+      props.variant === 'underline' && '-mb-px',
+      props.variant === 'default' && 'border-b border-gray-200 dark:border-gray-700',
+    )
   })
 
   const divClasses = computed(() => {
