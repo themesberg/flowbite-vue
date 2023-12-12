6801e255a3ec05190a6632e92b791cb1cf8f7df8
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2023-12-12 13:42:49 +0000

    Remove `aria-controls` attribute from `button` element

--- src/components/FwbSidebar/FwbSidebarDropdownItem.vue
+++ src/components/FwbSidebar/FwbSidebarDropdownItem.vue
@@ -3,7 +3,6 @@
     <button
       type="button"
       class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 z-10"
-      aria-controls="dropdown-example"
       @click="toggleDropdown"
     >
       <slot name="icon">
