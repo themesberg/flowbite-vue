79171e32bc401427d3ed80fadc3e05ecfef77656
Taha Okumuş <tahaokumus@hotmail.com>
2024-01-20 23:26:51 +0300

    fix: Fixed accordion content calculations

--- src/components/FwbAccordion/composables/useAccordionContentClasses.ts
+++ src/components/FwbAccordion/composables/useAccordionContentClasses.ts
@@ -4,20 +4,21 @@ import { twMerge } from 'tailwind-merge'
 
 const baseContentClasses = 'p-5 border border-gray-200 dark:border-gray-700 dark:bg-gray-900'
 
-export function useAccordionContentClasses (contentRef: Ref) {
+export function useAccordionContentClasses(contentRef: Ref) {
   const accordionId = computed(() => contentRef.value.parentElement.parentElement.dataset.accordionId)
   const panelId = computed(() => contentRef.value.parentElement.dataset.panelId)
   const { accordionsStates } = useAccordionState()
   const accordionState = computed(() => accordionsStates[accordionId.value])
   const panelState = computed(() => accordionsStates[accordionId.value].panels[panelId.value])
-  const panelsCount = computed(() => Object.keys(accordionsStates[accordionId.value].panels[panelId.value]).length)
+  const panelsCount = computed(() => Object.keys(accordionState.value.panels).length)
+  const isLastPanel = computed(() => panelState.value.order === panelsCount.value - 1)
 
   const contentClasses = computed(() => {
     return twMerge(
       baseContentClasses,
       !panelState.value.isVisible && 'hidden',
-      (panelState.value.order !== panelsCount.value - 1 || accordionState.value.flush) && 'border-b-0',
-      panelState.value.order === panelsCount.value - 1 && 'border-t-0',
+      (!isLastPanel.value || accordionState.value.flush) && 'border-b-0',
+      isLastPanel.value && 'border-t-0',
       accordionState.value.flush && 'border-x-0',
     )
   })
