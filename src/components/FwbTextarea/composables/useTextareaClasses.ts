a39cad36522fbbcded2d85991318cd70f943a9fb
Oğuzhan Güç <58819819+ogzcode@users.noreply.github.com>
2024-08-02 00:01:36 +0300

    Missing disabled style and doc code for textarea (#307)
    * chore: Add minlength and maxlength attributes to textarea component
    
    * chore: Update textarea default classes to include disabled styles
    
    ---------
    
    Co-authored-by: Oğuzhan Güç <oguzhan.guc@ayssoft.com>

--- src/components/FwbTextarea/composables/useTextareaClasses.ts
+++ src/components/FwbTextarea/composables/useTextareaClasses.ts
@@ -2,7 +2,7 @@ import { simplifyTailwindClasses } from '@/utils/simplifyTailwindClasses'
 import { computed } from 'vue'
 
 const textareaWrapperClasses = 'block w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600'
-const textareaDefaultClasses = 'block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-200 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+const textareaDefaultClasses = 'block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-200 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 disabled:cursor-not-allowed disabled:opacity-50'
 const textareaLabelClasses = 'block mb-2 text-sm font-medium text-gray-900 dark:text-white'
 const textareaFooterClasses = 'block py-2 px-3 border-gray-200 dark:border-gray-600'
 
