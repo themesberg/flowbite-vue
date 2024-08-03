0cc202f2b06749ebeabd35e62f2072f3da7e1516
Andrey Akulov <andreyjamer@users.noreply.github.com>
2024-08-03 05:07:02 +0500

    feat: Toggle label position (#296)
    * Fixed sizes for toggle components
    
    * Implemented option to swap the order of toggle and the label for Toggle component

--- src/components/FwbToggle/composables/useToggleClasses.ts
+++ src/components/FwbToggle/composables/useToggleClasses.ts
@@ -3,13 +3,25 @@ import type { InputSize } from '@/components/FwbInput/types'
 
 // Toggle Background
 const defaultLabelClasses = 'w-fit relative inline-flex items-center cursor-pointer'
-const defaultToggleBackgroundClasses = 'bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[""] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600'
-const defaultToggleBallClasses = 'ml-3 text-sm font-medium text-gray-900 dark:text-gray-300'
-const toggleSizeClasses: Record<InputSize, string> = {
-  lg: 'w-14 h-7 after:top-0.5 after:left-[4px] after:h-6 after:w-6',
-  md: 'w-11 h-6 after:top-[2px] after:left-[2px] after:h-5 after:w-5',
-  sm: 'w-9 h-5 after:top-[2px] after:left-[2px] after:h-4 after:w-4',
+const defaultToggleBackgroundClasses = 'relative bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[""] after:absolute after:bg-white after:border-gray-300 after:border after:rounded-full after:transition-all dark:border-gray-600 peer-checked:bg-blue-600'
+const defaultToggleBallClasses = 'text-sm font-medium text-gray-900 dark:text-gray-300'
+
+const labelOrderClasses: Record<string, string> = {
+  direct: '',
+  reverse: 'flex-row-reverse',
 }
+
+const toggleBallOrderClasses: Record<string, string> = {
+  direct: 'ms-3',
+  reverse: 'me-3',
+}
+
+const toggleSizeClasses: Record<InputSize, string> = {
+  lg: 'w-14 h-7 after:top-0.5 after:start-[4px] after:h-6 after:w-6',
+  md: 'w-11 h-6 after:top-[2px] after:start-[2px] after:h-5 after:w-5',
+  sm: 'w-9 h-5 after:top-[2px] after:start-[2px] after:h-4 after:w-4',
+}
+
 const toggleColorClasses: Record<string, string> = {
   red: 'peer-focus:ring-red-300 dark:peer-focus:ring-red-800 peer-checked:bg-red-600',
   green: 'peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:bg-green-600',
@@ -22,6 +34,7 @@ const toggleColorClasses: Record<string, string> = {
 export type UseToggleClassesProps = {
   size: Ref<InputSize>
   color: Ref<string>
+  reverse: Ref<boolean>
 }
 
 export function useToggleClasses (props: UseToggleClassesProps) {
@@ -30,6 +43,8 @@ export function useToggleClasses (props: UseToggleClassesProps) {
   const toggleSize = computed(() => toggleSizeClasses[props.size.value])
   const toggleColor = computed(() => toggleColorClasses[props.color.value])
   const toggleBallClasses = computed(() => defaultToggleBallClasses)
+  const toggleBallOrder = computed(() => toggleBallOrderClasses[props.reverse.value ? 'reverse' : 'direct'])
+  const labelOrder = computed(() => labelOrderClasses[props.reverse.value ? 'reverse' : 'direct'])
 
   return {
     labelClasses,
@@ -37,5 +52,7 @@ export function useToggleClasses (props: UseToggleClassesProps) {
     toggleClasses,
     toggleColor,
     toggleBallClasses,
+    toggleBallOrder,
+    labelOrder,
   }
 }
