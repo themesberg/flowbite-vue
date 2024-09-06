43db9b064ae4b6370b1bb0bb37172f4043c58029
Andrey Akulov <andreyjamer@gmail.com>
2024-09-06 12:23:24 +0500

    fix: FwbCard: Fixes after code review

--- src/components/FwbCard/composables/useCardClasses.ts
+++ src/components/FwbCard/composables/useCardClasses.ts
@@ -11,22 +11,22 @@ export function useCardsClasses (props: UseCardsClassesProps): {
   horizontalImageClasses: Ref<string>
 } {
   const cardClasses = computed(() => {
-    let classes = ''
+    let computedClasses = ''
     if (props.variant.value === 'image') {
-      classes = 'max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700'
+      computedClasses = 'max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700'
     }
 
     if (props.variant.value === 'default') {
-      classes = 'block max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 '
+      computedClasses = 'block max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 '
     } else if (props.variant.value === 'horizontal') {
-      classes = 'flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl dark:border-gray-700 dark:bg-gray-800'
+      computedClasses = 'flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl dark:border-gray-700 dark:bg-gray-800'
     }
 
     if (props.href?.value) {
-      classes += ' hover:bg-gray-100 dark:hover:bg-gray-700'
+      computedClasses += ' hover:bg-gray-100 dark:hover:bg-gray-700'
     }
 
-    return classes
+    return computedClasses
   })
 
   const horizontalImageClasses = computed(() => (props.variant.value === 'horizontal')
