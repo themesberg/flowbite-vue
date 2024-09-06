2c486c28674bfb8f42b43f20c4fe85a92ad20dff
Andrey Akulov <andreyjamer@gmail.com>
2024-09-06 11:05:40 +0500

    fix: FwbCard: Removed hover on non-links cards

--- src/components/FwbCard/composables/useCardClasses.ts
+++ src/components/FwbCard/composables/useCardClasses.ts
@@ -3,6 +3,7 @@ import type { CardsVariant } from '../types'
 
 export type UseCardsClassesProps = {
   variant: Ref<CardsVariant>
+  href?: Ref<string>
 }
 
 export function useCardsClasses (props: UseCardsClassesProps): {
@@ -10,15 +11,22 @@ export function useCardsClasses (props: UseCardsClassesProps): {
   horizontalImageClasses: Ref<string>
 } {
   const cardClasses = computed(() => {
-    if (props.variant.value === 'default') {
-      return 'block max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700'
-    } else if (props.variant.value === 'image') {
-      return 'max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700'
-    } else if (props.variant.value === 'horizontal') {
-      return 'flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'
+    let classes = ''
+    if (props.variant.value === 'image') {
+      classes = 'max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700'
     }
 
-    return ''
+    if (props.variant.value === 'default') {
+      classes = 'block max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 '
+    } else if (props.variant.value === 'horizontal') {
+      classes = 'flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl dark:border-gray-700 dark:bg-gray-800'
+    }
+
+    if (props.href?.value) {
+      classes += ' hover:bg-gray-100 dark:hover:bg-gray-700'
+    }
+
+    return classes
   })
 
   const horizontalImageClasses = computed(() => (props.variant.value === 'horizontal')
