ddc662053c9b035bd3e9850df195d145142bdfc9
Daniel Fahl <daniel.fahl@thomann.io>
2024-11-26 12:12:57 +0100

    chore: simplify button group with dropdown example

--- docs/components/buttonGroup/examples/FwbButtonGroupExampleDropdown.vue
+++ docs/components/buttonGroup/examples/FwbButtonGroupExampleDropdown.vue
@@ -1,21 +1,29 @@
 <template>
   <div class="vp-raw">
     <fwb-button-group>
-      <fwb-button> Button 1 </fwb-button>
-      <fwb-button> Button 2 </fwb-button>
+      <fwb-button>Button 1</fwb-button>
+      <fwb-button>Button 2</fwb-button>
       <fwb-dropdown text="Dropdown">
-        <p class="px-4">
+        <p class="p-2">
           This is a test dropdown
         </p>
       </fwb-dropdown>
       <fwb-dropdown text="Dropdown with list">
         <fwb-list-group>
-          <fwb-list-group-item
-            v-for="item in ['This', 'is', 'a', 'list']"
-            :key="item"
-            hover
-          >
-            <a href="#">{{ item }}</a>
+          <fwb-list-group-item hover>
+            <a href="#">These</a>
+          </fwb-list-group-item>
+          <fwb-list-group-item hover>
+            <a href="#">are</a>
+          </fwb-list-group-item>
+          <fwb-list-group-item hover>
+            <a href="#">some</a>
+          </fwb-list-group-item>
+          <fwb-list-group-item hover>
+            <a href="#">list</a>
+          </fwb-list-group-item>
+          <fwb-list-group-item hover>
+            <a href="#">items</a>
           </fwb-list-group-item>
         </fwb-list-group>
       </fwb-dropdown>
