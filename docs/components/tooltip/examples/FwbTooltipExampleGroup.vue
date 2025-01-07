1fa554f280bd3eafdefc9182b77d2a54f455d10d
tho-dan <daniel.fahl@thomann.io>
2025-01-07 16:03:13 +0100

    feat: add support for using tooltips on buttons that are inside a button group (#336)

---
+++ docs/components/tooltip/examples/FwbTooltipExampleGroup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="vp-raw">
+    <fwb-button-group>
+      <fwb-button>
+        Normal Button
+      </fwb-button>
+      <fwb-tooltip>
+        <template #trigger>
+          <fwb-button>
+            Button With Tooltip
+          </fwb-button>
+        </template>
+        <template #content>
+          Tooltip content
+        </template>
+      </fwb-tooltip>
+    </fwb-button-group>
+  </div>
+</template>
+
+<script setup>
+import { FwbButton, FwbButtonGroup, FwbTooltip } from '../../../../src/index'
+</script>
