b8651c4f6051b842948a36aa8fcc474954aabeeb
tho-dan <daniel.fahl@thomann.io>
2025-01-07 21:07:37 +0100

    Add support for grouping dropdowns and buttons together in button group (#331)
    * feat: add support for grouping dropdowns and buttons together in button group
    
    * feat: add documentation section for grouping buttons and dropdowns together
    
    * chore: simplify button group with dropdown example
    
    * chore: fix code style
    
    ---------
    
    Co-authored-by: Sqrcz <naorniakowski@slashlab.pl>

--- src/components/FwbButtonGroup/FwbButtonGroup.vue
+++ src/components/FwbButtonGroup/FwbButtonGroup.vue
@@ -9,14 +9,17 @@
 
 <style>
 .fwb-button-group > button,
+.fwb-button-group > .fwb-dropdown button,
 .fwb-button-group > .fwb-tooltip button {
   @apply rounded-none;
 }
 .fwb-button-group > button:first-child,
+.fwb-button-group > .fwb-dropdown:first-child button,
 .fwb-button-group > .fwb-tooltip:first-child button {
   @apply rounded-l-lg;
 }
 .fwb-button-group > button:last-child,
+.fwb-button-group > .fwb-dropdown:last-child button,
 .fwb-button-group > .fwb-tooltip:last-child button {
   @apply rounded-r-lg;
 }
