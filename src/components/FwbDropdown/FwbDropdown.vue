92012f70a02cd89a3ba62b795d4ff7d13bc8cf9b
Sqrcz <naorniakowski@slashlab.pl>
2025-01-16 09:55:45 +0100

    fix: triggerClass added to trigger element

--- src/components/FwbDropdown/FwbDropdown.vue
+++ src/components/FwbDropdown/FwbDropdown.vue
@@ -7,7 +7,7 @@
       <fwb-slot-listener @click="onToggle">
         <slot name="trigger">
           <fwb-button
-            :class="{'flex-row-reverse': placement === 'left', 'pl-2': placement === 'left'}"
+            :class="{'flex-row-reverse': placement === 'left', 'pl-2': placement === 'left', triggerClass}"
             :color="color"
             :disabled="disabled"
           >
