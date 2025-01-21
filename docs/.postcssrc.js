7fcd5896ba35960ddadb0b38fba1c72041a4cfff
Sqrcz <naorniakowski@slashlab.pl>
2025-01-21 16:02:18 +0100

    fix for CJS Node API deprecation

--- docs/.postcssrc.js
+++ docs/.postcssrc.js
@@ -3,16 +3,14 @@
 // here we add :not(:where(.vp-raw *)) selector to use it inside component examples
 // to prevent component style pollution from .vp-doc styles
 
-module.exports = {
-    plugins: {
-        'tailwindcss': {},
-        'postcss-prefix-selector': {
-            prefix: ':not(:where(.vp-raw *))',
-            includeFiles: [/vp-doc\.css/],
-            transform(prefix, _selector) {
-                const [selector, pseudo = ''] = _selector.split(/(:\S*)$/)
-                return selector + prefix + pseudo
-            }
-        },
-    }
+export const plugins = {
+    'tailwindcss': {},
+    'postcss-prefix-selector': {
+        prefix: ':not(:where(.vp-raw *))',
+        includeFiles: [/vp-doc\.css/],
+        transform(prefix, _selector) {
+            const [selector, pseudo = ''] = _selector.split(/(:\S*)$/)
+            return selector + prefix + pseudo
+        }
+    },
 }
