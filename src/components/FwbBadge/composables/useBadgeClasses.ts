476919b297e498a7117f8896b9308908bf632986
Steve Axtmann <steve.axtmann+github@gmail.com>
2025-01-31 13:14:40 +0100

    fix: dark mode badge colors to match flowbite design (#347)

--- src/components/FwbBadge/composables/useBadgeClasses.ts
+++ src/components/FwbBadge/composables/useBadgeClasses.ts
@@ -8,25 +8,25 @@ const badgeLinkClasses = 'bg-blue-100 hover:bg-blue-200 text-blue-800 dark:text-
 const onlyIconClasses = 'p-1 rounded-full mr-2'
 
 const badgeTextClasses: Record<BadgeType, string> = {
-  default: 'text-blue-800 dark:text-blue-800',
+  default: 'text-blue-800 dark:text-blue-300',
   dark: 'text-gray-800 dark:text-gray-300',
-  red: 'text-red-800 dark:text-red-900',
-  green: 'text-green-800 dark:text-green-900',
-  yellow: 'text-yellow-800 dark:text-yellow-900',
-  indigo: 'text-indigo-800 dark:text-indigo-900',
-  purple: 'text-purple-800 dark:text-purple-900',
-  pink: 'text-pink-800 dark:text-pink-900',
+  red: 'text-red-800 dark:text-red-300',
+  green: 'text-green-800 dark:text-green-300',
+  yellow: 'text-yellow-800 dark:text-yellow-300',
+  indigo: 'text-indigo-800 dark:text-indigo-300',
+  purple: 'text-purple-800 dark:text-purple-300',
+  pink: 'text-pink-800 dark:text-pink-300',
 }
 
 const badgeTypeClasses: Record<BadgeType, string> = {
-  default: 'bg-blue-100 dark:bg-blue-200',
+  default: 'bg-blue-100 dark:bg-blue-900',
   dark: 'bg-gray-100 dark:bg-gray-700',
-  red: 'bg-red-100 dark:bg-red-200',
-  green: 'bg-green-100 dark:bg-green-200',
-  yellow: 'bg-yellow-100 dark:bg-yellow-200',
-  indigo: 'bg-indigo-100 dark:bg-indigo-200',
-  purple: 'bg-purple-100 dark:bg-purple-200',
-  pink: 'bg-pink-100 dark:bg-pink-200',
+  red: 'bg-red-100 dark:bg-red-900',
+  green: 'bg-green-100 dark:bg-green-900',
+  yellow: 'bg-yellow-100 dark:bg-yellow-900',
+  indigo: 'bg-indigo-100 dark:bg-indigo-900',
+  purple: 'bg-purple-100 dark:bg-purple-900',
+  pink: 'bg-pink-100 dark:bg-pink-900',
 }
 
 const badgeSizeClasses: Record<BadgeSize, string> = {
