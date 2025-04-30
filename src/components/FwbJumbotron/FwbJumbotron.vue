33543127fafe65bd99620db448bdcdea8d52158c
Sqrcz <naorniakowski@slashlab.pl>
2025-04-30 18:33:22 +0200

    unnecessary eslint-disable removed

--- src/components/FwbJumbotron/FwbJumbotron.vue
+++ src/components/FwbJumbotron/FwbJumbotron.vue
@@ -46,18 +46,16 @@ const props = withDefaults(defineProps<IAlertProps>(), {
   headerClasses: '',
 })
 defineSlots<{
-  /* eslint-disable @typescript-eslint/no-explicit-any */
   default: any
-  /* eslint-enable @typescript-eslint/no-explicit-any */
 }>()
 
 const attrs = useAttrs()
 
 const wrapperClasses = computed(() => twMerge(
-  'bg-white dark:bg-gray-900 py-8 lg:py-16 px-4 mx-auto max-w-screen-xl text-center ',
+  'bg-white dark:bg-gray-900 mx-auto px-4 py-8 lg:py-16 max-w-screen-xl text-center',
   attrs.class as string))
 
-const headerClassesComputed = computed(() => twMerge('mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white', props.headerClasses))
-const subTextClassesComputed = computed(() => twMerge('mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-0 lg:px-16 dark:text-gray-400', props.subTextClasses))
+const headerClassesComputed = computed(() => twMerge('mb-4 font-extrabold text-gray-900 dark:text-white text-4xl md:text-5xl lg:text-6xl leading-none tracking-tight', props.headerClasses))
+const subTextClassesComputed = computed(() => twMerge('mb-8 sm:px-0 lg:px-16 font-normal text-gray-500 dark:text-gray-400 text-lg lg:text-xl', props.subTextClasses))
 
 </script>
