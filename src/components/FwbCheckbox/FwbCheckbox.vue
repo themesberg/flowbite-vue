86465931c09d07288df3c8ac5c1220f0dbddbaee
wazeerc <58399263+wazeerc@users.noreply.github.com>
2025-05-01 13:43:47 +0400

    fix: add select-none class to label in FwbCheckbox for better text selection handling

--- src/components/FwbCheckbox/FwbCheckbox.vue
+++ src/components/FwbCheckbox/FwbCheckbox.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="wrapperClass">
-    <label class="flex justify-start items-center">
+    <label class="flex justify-start items-center select-none">
       <input
         v-model="model"
         :class="checkboxClass"
