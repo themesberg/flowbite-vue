5465631b24b7196456daadb039ab0f08ff1ef293
wazeerc <58399263+wazeerc@users.noreply.github.com>
2025-07-19 19:05:17 +0400

    docs: add new custom example

--- docs/components/progress/examples/FwbProgressExampleColor.vue
+++ docs/components/progress/examples/FwbProgressExampleColor.vue
@@ -39,13 +39,6 @@
       label="Purple"
       :progress="100"
     />
-    <fwb-progress
-      :progress="50"
-      :inner-classes="'rounded-xs bg-teal-900 dark:bg-teal-200'"
-      :outer-classes="'bg-teal-200 dark:bg-teal-900'"
-      :outside-label-classes="'italic text-sky-800 dark:text-sky-200'"
-      label="Custom"
-    />
   </div>
 </template>
 
