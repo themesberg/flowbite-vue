8a303600eac15c24e4ee0264b9fbd1d9a1fbfb2b
Sqrcz <naorniakowski@slashlab.pl>
2026-02-04 13:33:35 +0100

    feat: add FwbFileInputDropzonePaceholder example component

---
+++ docs/components/fileInput/examples/FwbFileInputExampleDropZonePlaceholder.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="vp-raw">
+    <fwb-file-input
+      v-model="file"
+      dropzone
+    >
+      <template #dropzonePlaceholder>
+        <span class="font-semibold">Choose your file</span>
+        or drop it here
+      </template>
+    </fwb-file-input>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+import { FwbFileInput } from '../../../../src/index'
+
+const file = ref(null)
+</script>
