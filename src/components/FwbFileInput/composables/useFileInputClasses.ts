966e31e5e00b6af3a140991f6c7a65fc6ba84de4
Sqrcz <naorniakowski@slashlab.pl>
2026-02-04 13:41:26 +0100

    Update src/components/FwbFileInput/composables/useFileInputClasses.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/components/FwbFileInput/composables/useFileInputClasses.ts
+++ src/components/FwbFileInput/composables/useFileInputClasses.ts
@@ -4,7 +4,7 @@ import { useMergeClasses } from '@/composables/useMergeClasses'
 
 const fileInpDefaultClasses = 'block bg-gray-50 dark:bg-gray-700 px-2 py-1 border border-gray-300 dark:border-gray-600 rounded-lg focus:outline-none w-full text-gray-900 dark:text-gray-400 text-sm cursor-pointer dark:placeholder-gray-400'
 const fileInpLabelClasses = 'block mb-2 font-medium text-gray-900 dark:text-white text-sm'
-const fileInpDropzoneClasses = 'flex flex-col justify-center items-center bg-gray-50 hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-bray-800 dark:hover:bg-gray-600 border-2 border-gray-300 dark:border-gray-600 dark:hover:border-gray-500 border-dashed rounded-lg w-full h-64 cursor-pointer'
+const fileInpDropzoneClasses = 'flex flex-col justify-center items-center bg-gray-50 hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 border-2 border-gray-300 dark:border-gray-600 dark:hover:border-gray-500 border-dashed rounded-lg w-full h-64 cursor-pointer'
 const fileDropzoneWrapClasses = 'flex flex-col justify-center items-center pt-5 pb-6'
 const fileDropzoneDefaultTextClasses = '-mb-2! text-gray-500 dark:text-gray-400 text-sm'
 
