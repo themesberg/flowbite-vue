07170c4c16ad68ffa38d228b7229a15bbe2d5d79
Sqrcz <naorniakowski@slashlab.pl>
2026-06-10 23:03:22 +0200

    feat: extract useFormFieldIds composable and wire into FwbInput

---
+++ src/composables/useFormFieldIds.ts
@@ -0,0 +1,19 @@
+import { computed, type ComputedRef, useAttrs, useSlots } from 'vue'
+
+export const useFormFieldIds = (elementId: ComputedRef<string>) => {
+  const attrs = useAttrs()
+  const slots = useSlots()
+
+  const validationMessageId = computed(() => `${elementId.value}-validation`)
+  const helperId = computed(() => `${elementId.value}-helper`)
+
+  const ariaDescribedby = computed(() => {
+    const ids: string[] = []
+    if (attrs['aria-describedby']) ids.push(String(attrs['aria-describedby']))
+    if (slots.validationMessage) ids.push(validationMessageId.value)
+    if (slots.helper) ids.push(helperId.value)
+    return ids.length ? ids.join(' ') : undefined
+  })
+
+  return { ariaDescribedby, helperId, validationMessageId }
+}
